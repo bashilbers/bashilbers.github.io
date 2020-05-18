e1f2b3802439858a71ef97327ed788f736f935e9
Sebastiaan <sebastiaan@hilbers.tech>
2020-05-18 21:35:46 +0200

    Updates

---
+++ precache-manifest.77b64c486f19fea89e5f397f7a7b1a47.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "393b3556838a9f071ae234ac48774b64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26a2cb86705fdf12d78c",
+    "url": "/static/css/main.923d8c30.chunk.css"
+  },
+  {
+    "revision": "ee2cd0b68fbc4496e337",
+    "url": "/static/js/2.13e36d98.chunk.js"
+  },
+  {
+    "revision": "71772b085033e92f89cdfb3071333458",
+    "url": "/static/js/2.13e36d98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26a2cb86705fdf12d78c",
+    "url": "/static/js/main.94fc0f59.chunk.js"
+  },
+  {
+    "revision": "8c49fdbc71b0669b1102",
+    "url": "/static/js/runtime-main.781c15ab.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  }
+]);
